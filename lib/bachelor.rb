cf9e9913934e9d0cb4827975476901dccf07f8da
Janine Luk <janineluk95@gmail.com>
2018-11-17 23:33:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
     array.each do |person|
       person.each do |key,value|
         if key == "age"
-          age += value.to_i
+          age += value.ceil
           counter += 1  
         end
       end
