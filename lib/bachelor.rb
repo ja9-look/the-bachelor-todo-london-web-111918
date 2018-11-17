2de6c462419a1f1ee7d8104ac69d5acaa71c98a5
Janine Luk <janineluk95@gmail.com>
2018-11-17 23:18:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data[occupation].each do |person|
+  data.each do |season, array|
     person.each do |key, value|
       if v == occupation
         return person["name"]
